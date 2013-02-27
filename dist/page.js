4fc4d21d697bcd7a46f449ce94edc2c3deeeabe8
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2013-02-27 19:41:53 -0300

    bug fix for 1.5.1

--- dist/page.js
+++ dist/page.js
@@ -1807,7 +1807,7 @@ exports.Swf = Swf;
 		return {
 			create: function(data){
 				// mixin
-				data.id = data._id;
+				data.id = data._id || data.id;
 				data.src = data.file;
 				delete data.file;
 				delete data._id;
